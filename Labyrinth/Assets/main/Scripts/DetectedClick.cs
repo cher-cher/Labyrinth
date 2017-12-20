b6a3f80cc6541b6dd98654d48d824780962d38a0
cher-cher <chernykh.irina.1996@gmail.com>
2017-12-20 21:21:37 +0300

    Меню, персонаж и ar

---
+++ Labyrinth/Assets/main/Scripts/DetectedClick.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class DetectedClick : MonoBehaviour
+{
+    public Text playTxt;
+    public string level;
+
+    private bool clicked;
+
+    //void OnMouseDown()
+    //{
+    //    if (!clicked)
+    //    {
+    //        clicked = true;
+    //        SceneManager.LoadScene(level);
+    //    }
+    //}
+
+    void Update()
+    {
+        foreach(Touch t in Input.touches)
+		{
+			if(t.phase == TouchPhase.Began)
+			{
+				clicked = true;
+				SceneManager.LoadScene(level);
+			} 
+		}
+    }
+}
---
+++ Labyrinth/Assets/main/Scripts/TextFade.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TextFade : MonoBehaviour {
+
+	private Text txt;
+    
+    public int t;
+	
+	void Start () {
+        txt = GetComponent <Text> ();
+	}
+	
+	void Update () {
+		txt.color = new Color(txt.color.r, txt.color.g, txt.color.b, Mathf.PingPong(Time.time / 1.5f, t));
+	}
+}
